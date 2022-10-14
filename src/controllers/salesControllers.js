4148c454cc201d4a2b9c8a090205690d480585fb
Danilo Martins <dansm86@outlook.com>
2022-10-14 17:30:36 -0300

    req06-implemented route to add sales

---
+++ src/controllers/salesControllers.js
@@ -0,0 +1,23 @@
+const { createCustomError } = require('../errors/customError');
+const { salesServices } = require('../services');
+const mapError = require('../utils/errorMap');
+// const { resultMsg } = require('../utils/errorResults');
+
+const registerSales = async (req, res) => {
+  const salesList = req.body;
+  const { type, message } = await salesServices.resgisterSales(salesList);
+  if (type) throw createCustomError(message, mapError(type));
+  res.status(201).json(message);
+};
+
+const getSale = async (req, res) => {
+  const { saleId } = req.params;
+  const { type, message } = await salesServices.getSale(saleId);
+  if (type) throw createCustomError(message, mapError(type));
+  res.status(200).json(message);
+};
+
+module.exports = {
+  registerSales,
+  getSale,
+};
